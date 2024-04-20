2b79f4c1152a4f3cf071dcebe7166eea24c06f54
Tetosoan <113443635+Tetosoan@users.noreply.github.com>
2024-04-20 23:33:56 +0800

    Update App.java

--- App.java
+++ App.java
@@ -1,13 +1,21 @@
+import java.io.IOException;
+import java.net.URL;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.JFrame;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int boardWidth = 600;
+        int boardWidth = 1450;
         int boardHeight = boardWidth;
 
         JFrame frame = new JFrame("Snake");
         frame.setVisible(true);
-	frame.setSize(boardWidth, boardHeight);
+        frame.setSize(boardWidth, boardHeight);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -17,6 +25,19 @@ public class App {
         frame.pack();
         snakeGame.requestFocus();
 
-    
+        playMusicTest();
     }
+
+    private static void playMusicTest() throws UnsupportedAudioFileException {
+        try {
+            URL musicUrl = App.class.getResource("/Boombox3.wav");
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(musicUrl);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.start();
+        } catch (IOException | LineUnavailableException e) {
+            e.printStackTrace();
+            
+        }
+    } 
 }
